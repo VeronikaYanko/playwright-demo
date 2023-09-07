73673a48a75afad58ff644b912f3cdf9834e1c28
VeronikaYanko <103013096+VeronikaYanko@users.noreply.github.com>
2023-09-06 17:56:38 -0700

    first-commit

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+
+
