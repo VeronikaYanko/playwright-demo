d9ca6775584ec07cf15b0c9fec0f5925fb61c7ca
VeronikaYanko <103013096+VeronikaYanko@users.noreply.github.com>
2023-09-11 19:09:30 -0700

    add environment variables

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,15 +1,26 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test')
 
-test.describe('Authentication & Authorization', async () => {
-    test('Sign in with existing credentials', async ({ page}) => {
-        await page.goto('https://coding.pasv.us/user/login')
 
-        await page.locator('#normal_login_email').fill('veroyanko@gmail.com')
-        await page.locator('#normal_login_password').fill('Niiko4ka')
+test.describe('Authentication & Authorization', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('/user/login')
+    })
+
+    test('Sign in with existing credentials', async ({page}) => {
+        await page.locator('#normal_login_email').fill(process.env.EMAIL)
+        await page.locator('#normal_login_password').fill(process.env.PASSWORD)
         await page.locator('button[type="submit"]').click()
 
-      await expect(page.locator('.ant-avatar-square')).toBeVisible()
+        await expect(page.locator('.ant-avatar-square')).toBeVisible()
+    })
 
+    test('Sign in with not existing credentials', async ({page}) => {
+        await page.locator('#normal_login_email').fill('invalid@example.com')
+        await page.locator('#normal_login_password').fill('invalid')
+        await page.locator('button[type="submit"]').click()
 
+        const toast = page.locator('.ant-notification-notice-message')
+        await expect(toast).toBeVisible()
+        await expect(toast).toHaveText('User login. Fail')
     })
 })
